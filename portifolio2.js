fcc5843ea9259a6db0ee3f6295a54af2b0be11b2
kevinmisati <kevinmosigisi2001@gmail.com>
2021-04-28 11:20:41 +0300

    sss

--- portifolio2.js
+++ portifolio2.js
@@ -27,118 +27,30 @@ navToggle.addEventListener("click", function () {
   });
   
 
-/*================================SLIDER==========================================*/
-/*========maim array========*/
-
-const container = document.querySelector(".project_list");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-
-
-/* const projects = [
-
+const projects = [
   {
-     image:
-      "images\\dan-dimmock-sNwnjxm8eTY-unsplash.jpg",
-    name: "Budget",
-    text:
-      "This website is important in plannning how you are going to spend your money, it requests the total amount of money you want spend, then you enter all your expenses one by one as it culculates the balance.",
-    alt: "budget image",
-    link: "../BudgetApp\\budget.html",
-  }, 
-
-  {
-    image:
-      "images\\allie-gouPqaau9Qo-unsplash.jpg",
-    name: "Conversion",
-    text:
-      "In this site you can convert Kelvin to Degrees celsius and viceversa, Degrees to radians and viceversa,and lastly pounds to kilograms and viceversa.",
-    alt: " conversion image ",
-    link: "ConversionApp\\converse.html",
+    href: "./hospital/html/hospital.html",
+    src: "./images/hospital.png",
   },
-  {
-    image:
-      "images\\charles-deluvio-GlavtG-umzE-unsplash - Copy.jpg",
-    name: "Calculator",
-    text:
-      "This is a basic culculator for simple operations, I built it using HTML,CSS and JavaScript. It is very basic but I will be adding more functionality.",
-    alt: "image of a culculator",
-    link: "culculator\\index.html",
-  },
-
- 
-
- 
 ];
 
 
-container.innerHTML=projects.map((project,index) => {
-  const { image, name, text, alt, link } = project;
-  let position = "next";
-  if (index === 0) {
-    position = "active";
-  } else if (index === projects.length - 1) {
-    position = "last";
-  } 
-  return `<div class="project ${position}">
-  <a href="${link}" >
-       <img src="${image}" alt="${alt}">
-</a>
-<h2 class="project_title"><a href="${link}">${name}</a></h2>
-<p class="project_body">${text}</p>
-</div>`;
-
-}).join(""); */
-
-  
-
-  
-
-
-
-  function startSlides (type) {
-    
-  
-   
-
-  
-   let active = document.querySelector(".active");
-  let last = document.querySelector(".last");
-  let next = active.nextElementSibling;
-  if (!next) {
-    next=container.firstElementChild;
-  }
-
-
-  active.classList.remove(["active"]);
-  next.classList.remove(["last"]);
-
-  
-
-  active.classList.add(["last"]);
-  next.classList.add(["active"]);
-  last.classList.add(["next"]);
-
-  if (type==="prev"){
-    active.classList.add("next");
-  
-    next=last.previousElementSibling
-    next.classList.add("last");
-    last.classList.add("active");
-  
-  }
- 
- 
-} 
 
 
 
 
 
 
-prevButton.addEventListener("click",()=> {startSlides("prev")}
-);
 
-nextButton.addEventListener("click",() => {startSlides("next")}
-);
-  
+
+
+
+
+
+
+
+
+
+
+
+
